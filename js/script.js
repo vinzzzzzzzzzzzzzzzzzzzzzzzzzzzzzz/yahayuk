a866c67eb7a28382b4e7c2987dbdd72d0127bdb8
vinzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz <lanadentaalvino@gmail.com>
2024-07-07 23:00:59 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// toggle class active
+const navbarNav = document.querySelector("navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("hamburger_menu").onclick = () => {
+  classlist.toggle("active");
+};
